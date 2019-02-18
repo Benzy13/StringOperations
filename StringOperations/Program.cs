afca32f4e8d8459135732e39fe613aa3464ebc68
Jean <Jean@CRONUS>
2019-02-18 17:11:56 +0200

    2nd commit

--- StringOperations/Program.cs
+++ StringOperations/Program.cs
@@ -10,7 +10,19 @@ namespace StringOperations
     {
         static void Main(string[] args)
         {
+            string fullName = "James Bond ";
+            Console.WriteLine("Trim: '{0}'" , fullName.Trim());
+            Console.WriteLine("ToUpper: '{0}'", fullName.Trim().ToUpper());
 
+            var index = fullName.IndexOf(' ');
+            var firstName = fullName.Substring(0, index);
+            var lastName = fullName.Substring(index + 1);
+            Console.WriteLine("First Name: " + firstName);
+            Console.WriteLine("Last Name: " + lastName);
+
+            var names = fullName.Split(' ');
+            Console.WriteLine("First Name: " + names[0]);
+            Console.WriteLine("Last Name: " + names[1]);
         }
     }
 }
